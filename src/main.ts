f2b11427edd834c41eb4965e05e880cf3e45f007
Misael Trincado <misatrincado@gmail.com>
2023-08-03 22:04:35 -0400

    fix port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { ENV } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.use(bodyParser.json({ limit: '50mb' }));
   app.enableCors();
-  await app.listen(process.env.PORT || 3500);
+  const port = ENV === 'development' ? 3500 : 80;
+  await app.listen(process.env.PORT || port);
 }
 bootstrap();
