bd35448a642ddae3a3edd7c184dc7641dbea9c62
Misael Trincado <misatrincado@gmail.com>
2023-08-07 12:09:30 -0400

    cert

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,15 @@ import * as bodyParser from 'body-parser';
 import { ENV } from './config';
 import * as fs from 'fs';
 
-const httpsOptions = {
-  key: fs.readFileSync('./src/secrets/private.key'),
-  cert: fs.readFileSync('./src/secrets/certificate.crt'),
-  ca: [fs.readFileSync('./src/secrets/ca_bundle.crt')],
-};
+// const httpsOptions = {
+//   key: fs.readFileSync('../certificados/server.key'),
+//   cert: fs.readFileSync('./certificados/certificate.crt'),
+//   ca: [fs.readFileSync('./certificados/ca_bundle.crt')],
+// };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule, {httpsOptions});
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.use(bodyParser.json({ limit: '50mb' }));
   app.enableCors();
