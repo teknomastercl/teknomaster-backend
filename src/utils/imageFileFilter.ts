04052c036eb99672797a28fd07e9d26f71d7ce7f
Misael Trincado <misatrincado@gmail.com>
2023-11-01 12:23:10 -0300

    fix error

--- src/utils/imageFileFilter.ts
+++ src/utils/imageFileFilter.ts
@@ -1,6 +1,8 @@
+import { errorSend } from "./errorSend";
+
 export const imageFileFilter = (req, file, callback) => {
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    return callback(new Error('Solo se permite JPG, JPEG, PNG, GIF'), false);
+    return callback(errorSend(1, 'Solo se permite JPG, JPEG, PNG, GIF'), false);
   }
   callback(null, true);
 };
