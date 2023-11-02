4d81381fe30875f8a19986397ce5a5d88cce7e03
Misael Trincado <misatrincado@gmail.com>
2023-11-01 21:50:13 -0300

    product client

--- src/pre-customer-product/pre-customer-product.controller.ts
+++ src/pre-customer-product/pre-customer-product.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import { PreCustomerProductService } from './pre-customer-product.service';
 
 @Controller('pre-customer-product')
-export class PreCustomerProductController {}
+export class PreCustomerProductController {
+  constructor(private readonly service: PreCustomerProductService) {}
+
+  @Get('/pre-customer/:id')
+  async findByUserId(@Param('id') id) {
+    const data = await this.service.findByPreCustomer(id);
+    return {
+      data,
+    };
+  }
+
+  @Put()
+  async update(@Body() dto) {
+    const data = await this.service.update(dto);
+    return data;
+  }
+}
